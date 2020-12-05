d7815a083608cf99e5182921b1d3aefdba3136d6
Michael Fuget <michaelfuget@gmail.com>
2020-12-05 19:35:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(open(html))
 puts "test"
-courses =  doc.css(.inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+courses =  doc.css("".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
 courses.each do |course|
   puts course
 end
